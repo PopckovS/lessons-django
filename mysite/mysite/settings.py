44e3bd746b1fb9dfaa1af70118530314e8cda483
PopckovS <popckovM5@yandex.com>
2021-02-02 04:42:24 +0300

    Первый запуск админки и применение миграций

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': 'admin',
+        # 'PASSWORD': 'pass',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '8000',
     }
 }
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    print('777');
-    print(request);
-    print('777');
     return HttpResponse("Hello, world. You're at the polls index.")
