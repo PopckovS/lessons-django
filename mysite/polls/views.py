93ba13ca832e072e13789c9d712e00fdf8765b14
PopckovS <popckovM5@yandex.com>
2021-02-07 05:15:14 +0300

    Добавил разные виды

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
+
+# Главный метод, исполняется по дефолту, показывает все вопросы
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+# Показать конкретный вопрос по его id
+def detail(request, question_id):
+    return HttpResponse("Показать конкретный вопрос по его id =  %s." % question_id)
+
+
+# Показать рузультат по конкретному вопросу по его id
+def results(request, question_id):
+    response = "Показать рузультат по конкретному вопросу по его id =  %s."
+    return HttpResponse(response % question_id)
+
+
+# Голосование по вопросу
+def vote(request, question_id):
+    return HttpResponse("Голосование по вопросу %s." % question_id)
+
+
+
+
+
+
+
+
